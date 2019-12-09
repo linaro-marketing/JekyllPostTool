4a75197b2624512d1613e56bf2cd95f69236fc39
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-12-09 15:43:10 +0000

    Updates to the sched.py example

--- examples/sched.py
+++ examples/sched.py
@@ -75,60 +75,46 @@ class ConnectSchedJekyllPosts:
         """
         for session in sessions_data:
             # Grab the relevant data from the sessions results
-            sched_event_id = session["event_key"]
-            session_active = session["active"]
             session_title = session["name"]
             session_start_time = session["event_start"]
             session_end_time = session["event_end"]
-            try:
-                session_track = session["event_type"]
-            except Exception as e:
-                session_track = None
-            try:
-                session_sub_track = session["event_subtype"]
-            except KeyError as e:
-                session_sub_track = None
-            try:
-                session_abstract = session["description"]
-            except Exception as e:
-                session_abstract = "Coming soon..."
-            session_attendee_num = session["goers"]
             session_private = session["invite_only"]
-            try:
+            session_track = None
+            session_room = None
+            session_sub_track = None
+            session_speakers = None
+            session_abstract = "Coming soon"
+            if "event_type" in session:
+                session_track = session["event_type"]
+            if "event_subtype" in session:
+                session_sub_track = session["event_subtype"]
+            if "description" in session:
+                session_abstract = session["description"]
+            if "venue" in session:
                 session_room = session["venue"]
-            except Exception as e:
-                session_room = None
-            try:
-                session_venue_id = session["venue_id"]
-            except Exception as e:
-                pass
-            session_id_hash = session["id"]
-            try:
+            if "speakers" in session:
                 session_speakers = session["speakers"].split(",")
-            except KeyError as e:
-                session_speakers = None
 
+            # Used to determine whether to skip this session
+            skip_session = False
+            # Check the session track is not in the blacklisted tracks
             if session_track not in self.blacklistedTracks:
                 # Get the session id from the title
-                try:
-                    session_id_regex = re.compile(
-                        'SAN19-[A-Za-z]*[0-9]+K*[0-9]*')
-                    session_id = session_id_regex.findall(session_title)[0]
+                session_id_regex = re.compile(
+                    'SAN19-[A-Za-z]*[0-9]+K*[0-9]*')
+                session_id = session_id_regex.findall(session_title)
+                if len(session_id) == 0:
+                    skip_session = True
+                else:
+                    session_id = session_id[0]
                     session_name = re.sub(
                         "SAN19-[A-Za-z]*[0-9]+K*[0-9]*", "", session_title).strip()
-
-                    skipping = False
-
-                # Check to see if a session id exists in the title
-                # if not then skip this session - marking as invalid if no session id is present.
-                except Exception as e:
-                    skipping = True
             else:
-                skipping = True
+                skip_session = True
 
-            if skipping == False:
+            if not skip_session:
                 # Gather the session speakers details
-                if session_speakers is not None:
+                if session_speakers:
                     session_speakers_arr = []
                     for speaker in session_speakers:
                         speaker_name = speaker.strip()
@@ -145,22 +131,22 @@ class ConnectSchedJekyllPosts:
                 revised_speakers = []
                 if session_speakers_arr != None:
                     for speaker in session_speakers_arr:
-                        # Gets the speaker bio
                         speaker_details = self.get_speaker_bio(speaker)
-                        try:
-                            speaker_url = speaker['url']
-                        except KeyError:
-                            speaker_url = ""
+                        speaker_url = ""
+                        if "url" in speaker:
+                            speaker_url = speaker["url"]
+
                         revised_speakers.append({
                             "speaker_name": speaker_details["name"],
                             "speaker_username": speaker_details["username"],
-                            "speaker_url": speaker_details['url'],
+                            "speaker_url": speaker_url,
                             "speaker_company": speaker_details["company"],
                             "speaker_position": speaker_details["position"],
                             "speaker_location": speaker_details["location"],
                             "speaker_image": speaker_details["image"],
                             "speaker_bio":  "{}".format(speaker_details["bio"]).replace("'", ""),
                         })
+
 
                 session_image = {
                     "path": "/assets/images/featured-images/san19/" + session_id + ".png",
@@ -188,7 +174,6 @@ class ConnectSchedJekyllPosts:
                     "tags": session_tracks,
                     "categories": [self.connect_code],
                     "session_track": session_track,
-                    "session_attendee_num": session_attendee_num,
                     "tag": "session",
                 }
 
